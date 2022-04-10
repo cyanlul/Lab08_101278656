03459782f7fa4cfee38df36e1e8e2386762c8c9c
Dilan <dilan.roessell@gmail.com>
2022-04-10 14:52:51 -0400

    first commit

---
+++ exercise-1/student-app/src/app/students.component.ts
@@ -0,0 +1,20 @@
+import { DatePipe } from "@angular/common";
+import { Component } from "@angular/core";
+import { formatDate } from "@angular/common";
+
+@Component({
+    selector: 'students',
+    template: '<h2>{{getTitle()}} - {{getCurrentDate()}}</h2>'
+})
+
+export class StudentsComponent {
+    title = "My List of Students";
+
+    getTitle(){
+        return this.title;
+    }
+
+    getCurrentDate() {
+        return formatDate(new Date(), 'yyyy/MM/dd', 'en')
+    }
+}
